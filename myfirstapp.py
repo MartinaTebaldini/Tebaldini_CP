6358cccdb9a5903fcbad315047a6ba092bda7ce4
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-21 16:02:27 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -12,28 +12,15 @@ else:
      
 import json, requests 
 
-# add your own APIkey
 APIkey = '629e7883de074fc948b601e927640e29'
 location = 'london'
 
-# check API documentation to see what structure of URL is needed to access the data
-# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
-# print(url)
 
 
-# Download the JSON data from OpenWeatherMap.org's API.
-response = requests.get(url)  
-# Uncomment to see the raw JSON text:
-# print(response.text)  
+response = requests.get(url)   
 
-
-# Load JSON data into a Python variable.
 weatherData = json.loads(response.text)
-# Uncomment to see the raw JSON text:
-# print(weatherData) 
-# from pprint import pprint 
-# pprint(weatherData) 
 
 print(weatherData['main']['temp_max']) 
-# more???????????'
+

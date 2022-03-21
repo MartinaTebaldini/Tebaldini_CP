3043ad09123230f7628698548e7cd923d2b4439c
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-21 16:46:34 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -30,7 +30,7 @@ import json, requests
 APIkey = '629e7883de074fc948b601e927640e29'
 #location = st.text_input("give me a city name ")
 #location = st.text_input("give me a city name between milan, bolzano and palermo")
-
+location = st.radio("Choose one of the following citys",('Milan', 'Bolzano', 'Palermo'), help = "clik one of the three options")
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric'
 
 # Download the JSON data from OpenWeatherMap.org's API.

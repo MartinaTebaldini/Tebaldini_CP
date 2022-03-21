3879950da62963100e491cc5c89a2ca93d46bde8
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-21 16:44:57 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -29,7 +29,7 @@ import json, requests
 
 APIkey = '629e7883de074fc948b601e927640e29'
 #location = st.text_input("give me a city name ")
-location = st.text_input("give me a city name between milan, bolzano and palermo")
+#location = st.text_input("give me a city name between milan, bolzano and palermo")
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric'
 
@@ -48,10 +48,10 @@ weatherData = json.loads(response.text)
 
 #-----------------------
 
-weather = st.radio("Choose one of the following citys",('Milan', 'Bolzano', 'Palermo'), help = "clik one of the three options")
-if weather == 'Milan':
+location = st.radio("Choose one of the following citys",('Milan', 'Bolzano', 'Palermo'), help = "clik one of the three options")
+if location == 'Milan':
      st.write(weatherData)
-elif weather == 'Bolzano':
+elif location == 'Bolzano':
      st.write(weatherData)
 else:
      st.write(weatherData)

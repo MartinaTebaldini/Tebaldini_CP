0d76aa360593cfbbf077833aaa631f9814742d70
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-21 16:39:57 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -50,7 +50,7 @@ weatherData = json.loads(response.text)
 
 weather = st.radio("Choose one of the following citys",('Milan', 'Bolzano', 'Palermo'), help = "clik one of the three options")
 if weather == 'Milan':
-     st.write(weatherData))
+     st.write(weatherData)
 elif weather == 'Bolzano':
      st.write(weatherData)
 else:

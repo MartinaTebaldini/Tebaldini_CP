f289304e71de09df0fa2b4c22b114e93da4272c2
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-21 15:58:20 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -9,3 +9,31 @@ if genre == 'Comedy':
      st.write('You selected comedy.')
 else:
      st.write("You didn't select comedy.")
+     
+import json, requests 
+
+# add your own APIkey
+APIkey = '629e7883de074fc948b601e927640e29'
+location = 'london'
+
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+# print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+# print(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+print(weatherData['main']['temp_max']) 
+# more???????????'

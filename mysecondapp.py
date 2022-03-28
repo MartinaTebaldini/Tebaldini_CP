8f447013eaf3fd453ea4fbb05079e4b7a55f19e1
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-28 16:43:44 +0200

    Update mysecondapp.py

--- mysecondapp.py
+++ mysecondapp.py
@@ -4,7 +4,7 @@ keyword=st.text_input('plz give me a keyword')
 
 synonims = 'https://api.datamuse.com/words?rel_syn='+ keyword + '&max=4'
 antonyms = 'https://api.datamuse.com/words?rel_ant='+ keyword + '&max=4'
-meaning = 'https://api.datamuse.com/words?ml'+ keyword + '&max=4'
+meaning = 'https://api.datamuse.com/words?ml='+ keyword + '&max=4'
 
 option = st.selectbox('What would you like to know about this word?', ('synonims', 'antonyms', 'meaning'))
 st.write('You selected:', option)

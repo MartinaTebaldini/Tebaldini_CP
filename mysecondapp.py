aacad16cff3d1dd6c8fdfdb49804a6ed4c330083
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-28 16:17:30 +0200

    Create mysecondapp.py

---
+++ mysecondapp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+keyword=st.text_input('plz give me a keyword')
+
+synonims = 'https://api.datamuse.com/words?rel_syn='+ keyword + '&max=4'
+antonyms = 'https://api.datamuse.com/words?rel_ant='+ keyword + '&max=4'
+
+option = st.selectbox('What would you like to know about this word?', ('synonims', 'antonyms'))
+st.write('You selected:', option):
+
+
+#st.header("hello world")  
+#st.text("from Brixen")
+
+#st.write('The current movie title is', title)

13b254818a837c1e5429a5daeb7280a24a7edadb
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-28 16:42:46 +0200

    Update mysecondapp.py

--- mysecondapp.py
+++ mysecondapp.py
@@ -4,25 +4,27 @@ keyword=st.text_input('plz give me a keyword')
 
 synonims = 'https://api.datamuse.com/words?rel_syn='+ keyword + '&max=4'
 antonyms = 'https://api.datamuse.com/words?rel_ant='+ keyword + '&max=4'
+meaning = 'https://api.datamuse.com/words?ml'+ keyword + '&max=4'
 
-option = st.selectbox('What would you like to know about this word?', ('synonims', 'antonyms'))
+option = st.selectbox('What would you like to know about this word?', ('synonims', 'antonyms', 'meaning'))
 st.write('You selected:', option)
 
 
 response1 = requests.get(synonims)
-response2 = requests.get(antonyms)
+response2 = requests.get(meaning)
+response3 = requests.get(antonyms)
 dataFromDatamuse1 = json.loads(response1.text)
 dataFromDatamuse2 = json.loads(response2.text)
+dataFromDatamuse3 = json.loads(response3.text)
 
 if option == "synonims":
   st.write(dataFromDatamuse1)
-else:
+elif option == "meaning":
   st.write(dataFromDatamuse2)
+else:
+  st.write(dataFromDatamuse3)
   
-#response1 = requests.get(url)
-#response2 = requests.get(url2)
-#dataFromDatamuse1 = json.loads(response1.text)
-#dataFromDatamuse2 = json.loads(response2.text)
+
 
 
 

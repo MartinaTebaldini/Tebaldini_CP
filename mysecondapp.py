8e64b6502f169bebb12afbd02f0596c46fdc83f1
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-28 16:19:40 +0200

    Update mysecondapp.py

--- mysecondapp.py
+++ mysecondapp.py
@@ -5,7 +5,7 @@ synonims = 'https://api.datamuse.com/words?rel_syn='+ keyword + '&max=4'
 antonyms = 'https://api.datamuse.com/words?rel_ant='+ keyword + '&max=4'
 
 option = st.selectbox('What would you like to know about this word?', ('synonims', 'antonyms'))
-st.write('You selected:', option):
+st.write('You selected:', option)
 
 
 #st.header("hello world")  

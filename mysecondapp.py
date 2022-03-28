5ae014862c0dbef9b2ebd8ec52512415dabaf2a1
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-28 16:22:25 +0200

    Update mysecondapp.py

--- mysecondapp.py
+++ mysecondapp.py
@@ -7,6 +7,18 @@ antonyms = 'https://api.datamuse.com/words?rel_ant='+ keyword + '&max=4'
 option = st.selectbox('What would you like to know about this word?', ('synonims', 'antonyms'))
 st.write('You selected:', option)
 
+response1 = requests.get(synonims)
+response2 = requests.get(antonyms)
+dataFromDatamuse1 = json.loads(response1.text)
+dataFromDatamuse2 = json.loads(response2.text)
+
+print("you give me the word", keyword, "the word its synonims are")
+for eachentry in dataFromDatamuse2:
+  print("--", eachentry['word'])
+print("its antonyms are:")
+for eachentry in dataFromDatamuse1:
+  print("--", eachentry['word'])
+
 
 #st.header("hello world")  
 #st.text("from Brixen")

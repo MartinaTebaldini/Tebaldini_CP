dc1adf5f2b8d2e65d61d93129da462cbca7e8446
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-03-28 16:50:46 +0200

    Update mysecondapp.py

--- mysecondapp.py
+++ mysecondapp.py
@@ -1,28 +1,25 @@
 import streamlit as st
 import json, requests 
+from pprint import pprint
 keyword=st.text_input('plz give me a keyword')
 
-synonims = 'https://api.datamuse.com/words?rel_syn='+ keyword + '&max=4'
-antonyms = 'https://api.datamuse.com/words?rel_ant='+ keyword + '&max=4'
-meaning = 'https://api.datamuse.com/words?ml='+ keyword + '&max=4'
 
 option = st.selectbox('What would you like to know about this word?', ('synonims', 'antonyms', 'meaning'))
 st.write('You selected:', option)
 
 
-response1 = requests.get(synonims)
-response2 = requests.get(meaning)
-response3 = requests.get(antonyms)
-dataFromDatamuse1 = json.loads(response1.text)
-dataFromDatamuse2 = json.loads(response2.text)
-dataFromDatamuse3 = json.loads(response3.text)
-
 if option == "synonims":
-  st.write(dataFromDatamuse1['word'])
+  st.write('https://api.datamuse.com/words?rel_syn='+ keyword + '&max=4')
 elif option == "meaning":
-  st.write(dataFromDatamuse2['word'])
+  st.write('https://api.datamuse.com/words?ml='+ keyword + '&max=4')
 else:
-  st.write(dataFromDatamuse3['word'])
+  st.write('https://api.datamuse.com/words?rel_ant='+ keyword + '&max=4')
+  
+response = requests.get(url)   
+
+
+dataFromDatamuse = json.loads(response.text) 
+pprint(dataFromDatamuse[0:10]
   
 
 

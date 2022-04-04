8dca6df048cf0a1db011f2669704092923750ecf
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-04 16:50:03 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,8 +5,8 @@ translator = Translator()
 st.header("here is your translation!")
 word = st.text_input("gimme a word or phrase", "")
 if word != "":
- tar_language = st.text_input("give me a target language, it should be a two letter word, like es or de: ")
- languaget = translator.translate(word, dest = tar_language)
+ #tar_language = st.text_input("give me a target language, it should be a two letter word, like es or de: ")
+ languaget = translator.translate(word, dest = "it")
 
 st.write("the translation of your word is", languaget.text)
  

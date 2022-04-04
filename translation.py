cf93c49b1707ebd36ff15f22b43556eebac240b2
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-04 16:40:40 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,8 @@ from googletrans import Translator
 
 translator = Translator()
 word = st.text_input("gimme a word or phrase")
-languaget = translator.translate(word, dest = "it")
+tar_language = input("give me a target language, it should be a two letter word, like es or de: ")
+languaget = translator.translate(word, dest = tar_language)
 
-st.write(languaget.text)
+st.write("the translation of youe word is", languaget.text)
  

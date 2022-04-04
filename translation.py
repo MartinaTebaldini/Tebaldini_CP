4ee4badcb7cef1e4540015e94215923f62134492
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-04 16:11:26 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,8 @@
 #import streamlit as st
 from googletrans import Translator
-#translator = Translator()
-#word = True
-#while word != "nothing":
-  #word = st.text_input("gimme a word or phrase ")
- # langaugeis = translator.translate(word, dest = "it")
- # st.write(langaugeis)
-
-
-#import json, requests 
-#from pprint import pprint
+translator = Translator()
+word = True
+word = input("gimme a word or phrase")
+langaugeis = translator.translate(word, dest = "it")
+s.write(langaugeis.text)
 

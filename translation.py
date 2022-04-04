6a33ccd5264ce70ebcaa45bcad177693d656b8ea
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-04 16:45:11 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,10 +1,9 @@
-import streamlit as st
 from googletrans import Translator
-
+import streamlit as st
 translator = Translator()
 word = st.text_input("gimme a word or phrase")
 tar_language = st.text_input("give me a target language, it should be a two letter word, like es or de: ")
 languaget = translator.translate(word, dest = tar_language)
 
-st.write("the translation of youe word is", languaget.text)
+st.write("the translation of your word is", languaget.text)
  

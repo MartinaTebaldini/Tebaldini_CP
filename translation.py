cd87f2562f63a341d02c0fa89384b8a2238e124b
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-04 16:12:28 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,8 +1,7 @@
 #import streamlit as st
 from googletrans import Translator
 translator = Translator()
-word = True
-word = st.input_("gimme a word or phrase")
+word = st.text_input("gimme a word or phrase")
 langaugeis = translator.translate(word, dest = "it")
 s.write(langaugeis.text)
 

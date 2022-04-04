b293a7bd54381bfcf373418fed8cf9dc2cc6a361
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-04 16:49:14 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,10 +2,11 @@ from googletrans import Translator
 import streamlit as st
 translator = Translator()
 
-st.head("here is your translation!")
-word = st.text_input("gimme a word or phrase")
-tar_language = st.text_input("give me a target language, it should be a two letter word, like es or de: ")
-languaget = translator.translate(word, dest = tar_language)
+st.header("here is your translation!")
+word = st.text_input("gimme a word or phrase", "")
+if word != "":
+ tar_language = st.text_input("give me a target language, it should be a two letter word, like es or de: ")
+ languaget = translator.translate(word, dest = tar_language)
 
 st.write("the translation of your word is", languaget.text)
  

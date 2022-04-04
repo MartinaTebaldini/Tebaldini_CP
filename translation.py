56d8415cb59900bd58a92f9866bdd321509a242e
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-04 16:11:59 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,7 +2,7 @@
 from googletrans import Translator
 translator = Translator()
 word = True
-word = input("gimme a word or phrase")
+word = st.input_("gimme a word or phrase")
 langaugeis = translator.translate(word, dest = "it")
 s.write(langaugeis.text)
 

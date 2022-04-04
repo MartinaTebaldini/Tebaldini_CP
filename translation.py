e5ddeb73c3db327e6cddb7a2a1bdeeea7fb995f4
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-04 16:33:42 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,11 +1,9 @@
 import streamlit as st
-import json,requests
 from googletrans import Translator
-#import json, requests 
-#from pprint import pprint
+
 translator = Translator()
 word = st.text_input("gimme a word or phrase")
-langaugeis = translator.translate(word, dest = "it")
+languaget = translator.translate(word, dest = "it")
 
-st.write(langaugeis.text)
+st.write(languaget.text)
  

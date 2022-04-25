4c258802d7bf83db3bba4b2ecf5aaca358adbf98
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-25 17:36:12 +0200

    Create mediawiki.py

---
+++ mediawiki.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+import json, requests 
+from pprint import pprint
+keyword = True
+while keyword != "nothing":
+  keyword = st.text_input("what you want to know about ")
+  url ='https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='+keyword
+  st.write(url)
+  response = requests.get(url) 
+  dataFromWikipedia = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+#print(dataFromWikipedia) 
+# Uncomment to see a better readable JSON text:
+#pprint(dataFromWikipedia) #dont forget to import the correct library to make this work
+  textwithHTMLtags = dataFromWikipedia["query"]["search"][0]["snippet"]
+  from bs4 import BeautifulSoup
+  soup = BeautifulSoup(textwithHTMLtags)
+
+ 
+  textwithoutHTMLtags = soup.get_text()
+  print(textwithoutHTMLtags)

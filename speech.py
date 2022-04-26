24f122aba8f2df9b1c89876a5e220e3865727fa3
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-26 12:10:23 +0200

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from googletrans import Translator
-
 from gtts import gTTS
  
-translator = Translator()
+
 st.header("here is your translation and its pronunciation!")
 
 text_user = st.text_input("give a me text to translate ")
 tar_lang = st.text_input("give me a target language ")
 
+translator = Translator()
 tra_text = translator.translate(text_user, dest = tar_lang)
 
 tts1=gTTS(tra_text.text, tar_lang)

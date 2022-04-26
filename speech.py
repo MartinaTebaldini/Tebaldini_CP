1e96a0d9d7b9a43015d200ab98dcbd49126fd045
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-26 12:24:33 +0200

    Update speech.py

--- speech.py
+++ speech.py
@@ -5,13 +5,17 @@ from gtts import gTTS
 
 st.header("here is your translation and its pronunciation!")
 
-text_user = st.text_input("give a me text to translate ")
-tar_lang = st.text_input("give me a target language ")
+text_user = st.text_input("give a me text to translate ", " ")
 
-translator = Translator()
-tra_text = translator.translate(text_user, dest = tar_lang)
+if text_user == " ":
+ tar_lang = st.text_input("give me a target language ")
+ translator = Translator()
+ tra_text = translator.translate(text_user, dest = tar_lang)
 
 tts1=gTTS(tra_text.text, tar_lang)
-tts1.save("lang.mp3")
+#tts1.save("lang.mp3")
 #ipd.display(ipd.Audio("lang.mp3")
-st.audio("lang.mp3")
+#st.audio("lang.mp3")
+audio_file = open("myaudio.mp3", "rb")
+
+st.audio(data=audio_file, format="audio/mp3", start_time=0)

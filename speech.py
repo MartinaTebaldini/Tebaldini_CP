19d4d9486429d178d97113a843fc69d3dab7e532
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-26 12:02:44 +0200

    Update speech.py

--- speech.py
+++ speech.py
@@ -6,9 +6,9 @@ st.header("here is your translation and its pronunciation!")
 text_user = st.text_input("give a me text to translate ")
 tar_lang = st.text_input("give me a target language ")
 
-langaugeis = translator.translate(text_user, dest = tar_lang)
+tra_text = translator.translate(text_user, dest = tar_lang)
 
-tts1=gTTS(langaugeis.text, tar_lang)
+tts1=gTTS(tra_text.text, tar_lang)
 tts1.save("lang.mp3")
 #ipd.display(ipd.Audio("lang.mp3")
 st.audio("lang.mp3", autoplay=False)

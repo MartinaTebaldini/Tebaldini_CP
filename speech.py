de24dbe06520b7b767634a48511c31bd0d5cb1f6
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-26 11:56:00 +0200

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,13 +1,14 @@
 from googletrans import Translator
 import streamlit as st
+translator = Translator()
 st.header("here is your translation and its pronunciation!")
 
 text_user = st.text_input("give a me text to translate ")
 tar_lang = st.text_input("give me a target language ")
 
-langaugeis = translator.translate(text_user, dest = tar_lang)
+langaugeis = translator.translate(text_user,dest = tar_lang)
 
-tts1=gTTS(text = text_user, lang = tar_lang)
+tts1=gTTS(langaugeis.text, tar_lang)
 tts1.save("lang.mp3")
 #ipd.display(ipd.Audio("lang.mp3")
 st.audio("lang.mp3", autoplay=False)

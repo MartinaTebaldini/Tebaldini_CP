bad7f2392fbe8c8e7472813da596185b017ffad2
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-26 12:29:43 +0200

    Update speech.py

--- speech.py
+++ speech.py
@@ -13,9 +13,9 @@ if text_user == " ":
  tra_text = translator.translate(text_user, dest = tar_lang)
 
 tts1=gTTS(tra_text.text, tar_lang)
-#tts1.save("lang.mp3")
+tts1.save("lang.mp3")
 #ipd.display(ipd.Audio("lang.mp3")
 #st.audio("lang.mp3")
-audio_file = open("myaudio.mp3", "rb")
+audio_file = open("lang.mp3", "rb")
 
 st.audio(data=audio_file, format="audio/mp3", start_time=0)

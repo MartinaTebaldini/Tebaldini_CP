22018d08dab3513dff18a8b8a9328f14baf98853
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-04-26 12:35:49 +0200

    Update speech.py

--- speech.py
+++ speech.py
@@ -5,10 +5,10 @@ from gtts import gTTS
 
 st.header("here is your translation and its pronunciation!")
 
-text_user = st.text_input("give a me text to translate ")
+text_user = st.text_input("give a me text to translate ", " ")
 
 if text_user != "":
- tar_lang = st.text_input("give me a target language ")
+ tar_lang = st.text_input("give me a target language ", " ")
  translator = Translator()
  if tar_lang != "":
   tra_text = translator.translate(text_user, dest = tar_lang)

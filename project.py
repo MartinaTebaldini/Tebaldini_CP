329de9d3ac948f0cd74b870dae1db1be9e306fd7
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-05-09 21:03:21 +0200

    Update project.py

--- project.py
+++ project.py
@@ -13,21 +13,22 @@ import random
     
 
 uploaded_file = st.file_uploader("Please upload an Image file")
-extractedInformation = pytesseract.image_to_string(Image.open(uploaded_file)) #???????? how can i call the uploaded file in a general way?
-st.write(extractedInformation)
+if uploaded_file is not None:
+    extractedInformation = pytesseract.image_to_string(Image.open(uploaded_file)) #???????? how can i call the uploaded file in a general way?
+    st.write(extractedInformation)
 
-translator = Translator()
-detectedLanguage = translator.detect(extractedInformation)
-st.write(detectedLanguage) #to detect the language of the image
+    translator = Translator()
+    detectedLanguage = translator.detect(extractedInformation)
+    st.write(detectedLanguage) #to detect the language of the image
 
-st.write("here is a link with the language codes")
-url = "https://cloud.google.com/translate/docs/languages"
-st.write(url)
-tar_lang = st.text_input("in which language do you want to translate the text?")
-st.write("you selected", tar_lang)
+    st.write("here is a link with the language codes")
+    url = "https://cloud.google.com/translate/docs/languages"
+    st.write(url)
+    tar_lang = st.text_input("in which language do you want to translate the text?")
+    st.write("you selected", tar_lang)
 
-if tar_lang != "":
-    tra_text = translator.translate(extractedInformation, dest = tar_lang)
-    st.write(tra_text.text)
+    if tar_lang != "":
+        tra_text = translator.translate(extractedInformation, dest = tar_lang)
+        st.write(tra_text.text)
 
 

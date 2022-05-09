09c5711de093f8cb4f88c8eaf24b56f142e28853
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-05-09 15:28:31 +0200

    Update speech_rec.py

--- speech_rec.py
+++ speech_rec.py
@@ -2,11 +2,11 @@
 import speech_recognition as sr
 import streamlit as st
 import requests
-
-r = sr.Recognizer()
-#AUDIO_FILE = "sample_audio_short.wav"   #mp3 files are not supported
-AUDIO_FILE = "sample_audio_long.wav"    #these files are in files-section of Class08 folder, on MS Teams.
 if text_user != "":
+  r = sr.Recognizer()
+#AUDIO_FILE = "sample_audio_short.wav"   #mp3 files are not supported
+  AUDIO_FILE = "sample_audio_long.wav"    #these files are in files-section of Class08 folder, on MS Teams.
+
   AUDIO_FILE= st.file_uploader("choose a file") 
   if AUDIO_FILE is not None:
     st.write("you uploaded this file")

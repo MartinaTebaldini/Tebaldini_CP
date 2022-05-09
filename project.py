1bdead335773409797ec9a9d17bbd4302bd63b2e
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-05-09 17:28:41 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,6 @@
-
-
-
-
+import streamlit as st
+from googletrans import Translator
+from gtts import GTTS
 
 import pytesseract
 import shutil
@@ -11,3 +10,7 @@ try:
     from PIL import Image
 except ImportError:
     import Image
+
+uploaded_file = st.file_uploader("Choose a file")
+extractedInformation = pytesseract.image_to_string(Image.open(uploaded_file))
+st.write(extractedInformation)

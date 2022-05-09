7f3fd12cae78bb6437c8cfefedb3785530e2fd1c
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-05-09 15:27:31 +0200

    Update speech_rec.py

--- speech_rec.py
+++ speech_rec.py
@@ -6,17 +6,16 @@ import requests
 r = sr.Recognizer()
 #AUDIO_FILE = "sample_audio_short.wav"   #mp3 files are not supported
 AUDIO_FILE = "sample_audio_long.wav"    #these files are in files-section of Class08 folder, on MS Teams.
+if text_user != "":
+  AUDIO_FILE= st.file_uploader("choose a file") 
+  if AUDIO_FILE is not None:
+    st.write("you uploaded this file")
+    st.audio(AUDIO_FILE, format = "audio/wav")
+    with sr.AudioFile(AUDIO_FILE) as source:
+      audio = r.record(source)  # read the entire audio file
 
-AUDIO_FILE= st.file_uploader("choose a file") 
 
-if AUDIO_FILE is not None:
-  st.write("you uploaded this file")
-  st.audio(AUDIO_FILE, format = "audio/wav")
-  with sr.AudioFile(AUDIO_FILE) as source:
-    audio = r.record(source)  # read the entire audio file
+  recognised_text= r.recognize_google(audio)
 
-
-recognised_text= r.recognize_google(audio)
-
-st.write('the text recognized from the audio seems to be: ')
-st.write( recognised_text)
+  st.write('the text recognized from the audio seems to be: ')
+  st.write( recognised_text)

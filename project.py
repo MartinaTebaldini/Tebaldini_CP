81d9b2e457a467281ad28a4e5ea31b7276805e86
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-05-10 11:04:52 +0200

    Update project.py

--- project.py
+++ project.py
@@ -12,19 +12,19 @@ import random
 urlfoto = "https://raw.githubusercontent.com/MartinaTebaldini/Tebaldini_CP/main/lingue-straniere.jpg"
 st.image(urlfoto)
 
-st.header("Import an image and I will translate it for you!")
+st.title("Import an image and I will translate it for you!")
 uploaded_file = st.file_uploader("Please upload an Image file")
 if uploaded_file is not None:
     
     extractedInformation = pytesseract.image_to_string(Image.open(uploaded_file)) #???????? how can i call the uploaded file in a general way?
-    st.write("Here is the text extracted from the image")
+    st.header("Here is the text extracted from the image")
     st.write(extractedInformation)
 
     translator = Translator()
     #detectedLanguage = translator.detect(extractedInformation)
     #st.write(detectedLanguage) #to detect the language of the image
 
-    st.write("here is a link with the language codes, pick one!")
+    st.header("here is a link with the language codes, pick one!")
     url = "https://cloud.google.com/translate/docs/languages"
     st.write(url)
     tar_lang = st.text_input("in which language do you want to translate the text?")
@@ -32,9 +32,9 @@ if uploaded_file is not None:
 
     if tar_lang != "":
         tra_text = translator.translate(extractedInformation, dest = tar_lang)
-        st.write("here is the translation in", tar_lang)
+        st.header("here is the translation in", tar_lang)
         st.write(tra_text.text)
-        st.write("here is the pronunciation!", tra_text.pronunciation)
+        st.header("here is the pronunciation!", tra_text.pronunciation)
         tts1=gTTS(tra_text.text, tar_lang)
         tts1.save("lang.mp3")
         audio_file = open("lang.mp3", "rb")

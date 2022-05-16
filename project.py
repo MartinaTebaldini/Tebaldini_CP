5b93f79bad6168f4747271b2f667f8d495c23caa
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-05-16 16:41:48 +0200

    Update project.py

--- project.py
+++ project.py
@@ -46,7 +46,7 @@ if picture is not None:
         st.subheader("here is the translation in", tar_lang)
         st.write(tra_text.text)
         st.subheader("here is the pronunciation!", tra_text.pronunciation)
-        ttmp3=gTTS(tra_text.text, tar_lang, tld="com")
+        ttmp3=gTTS(tra_text.text, lang =tar_lang, tld="com")
         ttmp3.save("lang.mp3")
         audio_file = open("lang.mp3", "rb")
         st.audio(data=audio_file, format="audio/mp3", start_time=0)

ea0fcd3333d730344c4104aba0be59d9a7fb82d3
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-05-26 10:55:47 +0200

    Update project.py

--- project.py
+++ project.py
@@ -47,18 +47,20 @@ if picture is not None:
      tar_lang = st.text_input("in which language do you want to translate the text?")
     #st.write("you selected", tar_lang)
      
-    if tar_lang != "":
-         if tar_lang == detectedLanguage:
+     if tar_lang != "":
+          if tar_lang == detectedLanguage:
                st.write("the language you selected is the same as the detected one! Pick another one")
-         else:
-          tra_text = translator.translate(extractedInformation, dest = tar_lang)
-          st.write("here is the translation in", tar_lang)
-          st.write(tra_text.text)
-          st.write("here is the pronunciation!", tra_text.pronunciation)
-          ttmp3=gTTS(tra_text.text, lang =tar_lang, tld="com")
-          ttmp3.save("lang.mp3")
-          audio_file = open("lang.mp3", "rb")
-          st.audio(data=audio_file, format="audio/mp3", start_time=0)
+          else:
+               tra_text = translator.translate(extractedInformation, dest = tar_lang)
+               st.write("here is the translation in", tar_lang)
+               st.write(tra_text.text)
+               st.write("here is the pronunciation!", tra_text.pronunciation)
+               ttmp3=gTTS(tra_text.text, lang =tar_lang, tld="com")
+               ttmp3.save("lang.mp3")
+               audio_file = open("lang.mp3", "rb")
+               st.audio(data=audio_file, format="audio/mp3", start_time=0)
+               
+        
           
 
 

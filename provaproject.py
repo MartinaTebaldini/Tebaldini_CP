5a2938912089e617fe1b51c38661712e5315db80
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-06-06 16:25:11 +0200

    Update provaproject.py

--- provaproject.py
+++ provaproject.py
@@ -25,44 +25,45 @@ option = st.selectbox(
      ('browser', 'camera'))
 
 #st.write('You selected:', option)
-
-if option == "browser":
-     picture = st.file_uploader("Please upload an Image file")
-else:
-     picture = st.camera_input("take a picture!")
-  
-if picture is not None:
-     extractedInformation = pytesseract.image_to_string(Image.open(picture), lang='jpn+eng+hrv+ara+it') 
-     st.markdown("**Here is the text extracted from the image**")
-     st.write(extractedInformation)
-
-     translator = Translator()
-     detectedLanguage = translator.detect(extractedInformation)
-     if detectedLanguage is None:
-          st.write("I'm sorry, the language of the image has not been detected")
+if option != "":
+     
+     if option == "browser":
+          picture = st.file_uploader("Please upload an Image file")
      else:
-          st.write(detectedLanguage) #to detect the language of the image
+          picture = st.camera_input("take a picture!")
 
-          st.markdown("**here is a link with the language codes, please, pick one!**")
-          url = "https://cloud.google.com/translate/docs/languages"
-          st.write(url)
-          tar_lang = st.text_input("in which language do you want to translate the text?")
-         #st.write("you selected", tar_lang)
+     if picture is not None:
+          extractedInformation = pytesseract.image_to_string(Image.open(picture), lang='jpn+eng+hrv+ara+it') 
+          st.markdown("**Here is the text extracted from the image**")
+          st.write(extractedInformation)
 
-          if tar_lang != "":
-               if tar_lang == detectedLanguage:
-                    st.write("the language you selected is the same as the detected one! Pick another one")
-               else:
-                    tra_text = translator.translate(extractedInformation, dest = tar_lang)
-                    st.write("here is the translation in", tar_lang)
-                    st.write(tra_text.text)
-                    st.write("here is the pronunciation!", tra_text.pronunciation)
-                    ttmp3=gTTS(tra_text.text, lang =tar_lang, tld="com")
-                    ttmp3.save("lang.mp3")
-                    audio_file = open("lang.mp3", "rb")
-                    st.audio(data=audio_file, format="audio/mp3", start_time=0)
-               with st.expander("See credits"):
-                    st.write("""
-                    - for the language codes: https://cloud.google.com/translate/docs/languages
-                    - for the logo: https://www.inlinguaroma.com/quali-sono-le-lingue-piu-parlate-al-mondo/
-                    """)    
+          translator = Translator()
+          detectedLanguage = translator.detect(extractedInformation)
+          if detectedLanguage is None:
+               st.write("I'm sorry, the language of the image has not been detected")
+          else:
+               st.write(detectedLanguage) #to detect the language of the image
+
+               st.markdown("**here is a link with the language codes, please, pick one!**")
+               url = "https://cloud.google.com/translate/docs/languages"
+               st.write(url)
+               tar_lang = st.text_input("in which language do you want to translate the text?")
+              #st.write("you selected", tar_lang)
+
+               if tar_lang != "":
+                    if tar_lang == detectedLanguage:
+                         st.write("the language you selected is the same as the detected one! Pick another one")
+                    else:
+                         tra_text = translator.translate(extractedInformation, dest = tar_lang)
+                         st.write("here is the translation in", tar_lang)
+                         st.write(tra_text.text)
+                         st.write("here is the pronunciation!", tra_text.pronunciation)
+                         ttmp3=gTTS(tra_text.text, lang =tar_lang, tld="com")
+                         ttmp3.save("lang.mp3")
+                         audio_file = open("lang.mp3", "rb")
+                         st.audio(data=audio_file, format="audio/mp3", start_time=0)
+                    with st.expander("See credits"):
+                         st.write("""
+                         - for the language codes: https://cloud.google.com/translate/docs/languages
+                         - for the logo: https://www.inlinguaroma.com/quali-sono-le-lingue-piu-parlate-al-mondo/
+                         """)    

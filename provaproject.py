aafd6f449c23f4e7da3d396673d282d24cd7e6ca
MartinaTebaldini <101127728+MartinaTebaldini@users.noreply.github.com>
2022-06-06 15:53:09 +0200

    Update provaproject.py

--- provaproject.py
+++ provaproject.py
@@ -8,11 +8,23 @@ import shutil
 import os
 import random
 
+st.title("Import an image and I will translate it for you!")
+urlfoto = "https://raw.githubusercontent.com/MartinaTebaldini/Tebaldini_CP/main/lingue-straniere.jpg"
+st.image(urlfoto)
+st.write("This app converts images into texts, more specifically it reads and recognises texts in images, it detects the language and then the text can be translated into the language of the user's choice. As output, it will give both the translated text and audio")
+
+with st.expander("How to use"):
+     st.write("""
+         - Import an image
+         - Select the language you want translate the text into
+     """)
+
+
 option = st.selectbox(
-     'how would you like to import your image?',
+     'how would you like to import your image? Please select either browser or camera',
      ('browser', 'camera'))
 
-st.write('You selected:', option)
+#st.write('You selected:', option)
 
 if option == "browser":
      picture = st.file_uploader("Please upload an Image file")
@@ -20,27 +32,38 @@ else:
      picture = st.camera_input("take a picture!")
   
 if picture is not None:
-    
-    extractedInformation = pytesseract.image_to_string(Image.open(picture)) #???????? how can i call the uploaded file in a general way?
-    st.subheader("Here is the text extracted from the image")
-    st.write(extractedInformation)
+     extractedInformation = pytesseract.image_to_string(Image.open(picture), lang='jpn+eng+hrv+ara+it') 
+     st.markdown("**Here is the text extracted from the image**")
+     st.write(extractedInformation)
 
-    translator = Translator()
-    #detectedLanguage = translator.detect(extractedInformation)
-    #st.write(detectedLanguage) #to detect the language of the image
+translator = Translator()
+detectedLanguage = translator.detect(extractedInformation)
+if detectedLanguage is None:
+     st.write("I'm sorry, the language of the image has not been detected")
+else:
 
-    st.subheader("here is a link with the language codes, pick one!")
-    url = "https://cloud.google.com/translate/docs/languages"
-    st.write(url)
-    tar_lang = st.text_input("in which language do you want to translate the text?")
+     st.write(detectedLanguage) #to detect the language of the image
+
+     st.markdown("**here is a link with the language codes, please, pick one!**")
+     url = "https://cloud.google.com/translate/docs/languages"
+     st.write(url)
+     tar_lang = st.text_input("in which language do you want to translate the text?")
     #st.write("you selected", tar_lang)
 
-    if tar_lang != "":
-        tra_text = translator.translate(extractedInformation, dest = tar_lang)
-        st.subheader("here is the translation in", tar_lang)
-        st.write(tra_text.text)
-        st.subheader("here is the pronunciation!", tra_text.pronunciation)
-        tts1=gTTS(tra_text.text, tar_lang)
-        tts1.save("lang.mp3")
-        audio_file = open("lang.mp3", "rb")
-        st.audio(data=audio_file, format="audio/mp3", start_time=0)
+     if tar_lang != "":
+          if tar_lang == detectedLanguage:
+               st.write("the language you selected is the same as the detected one! Pick another one")
+          else:
+               tra_text = translator.translate(extractedInformation, dest = tar_lang)
+               st.write("here is the translation in", tar_lang)
+               st.write(tra_text.text)
+               st.write("here is the pronunciation!", tra_text.pronunciation)
+               ttmp3=gTTS(tra_text.text, lang =tar_lang, tld="com")
+               ttmp3.save("lang.mp3")
+               audio_file = open("lang.mp3", "rb")
+               st.audio(data=audio_file, format="audio/mp3", start_time=0)
+          with st.expander("See credits"):
+               st.write("""
+               - for the language codes: https://cloud.google.com/translate/docs/languages
+               - for the logo: https://www.inlinguaroma.com/quali-sono-le-lingue-piu-parlate-al-mondo/
+               """)    
